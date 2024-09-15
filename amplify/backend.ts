ba0b6a6c72b49e1549113c75a1b28dba67b2cd30
Kadyn-Jai Pearce <kadynjaipearce@gmail.com>
2024-09-15 17:23:40 +0800

    feat: add storage backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,8 @@
 import { defineBackend } from "@aws-amplify/backend";
 import { data } from "@/amplify/data/resource";
+import { storage } from "@/amplify/storage/resource";
 
 defineBackend({
   data,
+  storage,
 });
