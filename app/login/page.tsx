6fcad5e03923fdfd89b7d36ca716c472a2da3242
Kadyn-Jai Pearce <kadynjaipearce@gmail.com>
2024-09-30 22:36:07 +0800

    ♻️refactor: move login component into own page.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,67 @@
-import Login from "@/components/Login";
-import React from "react";
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { signIn } from "aws-amplify/auth";
+import { RiAccountCircleLine } from "react-icons/ri";
+import Container from "@/components/Container";
 
-export default function page() {
+export default function Page() {
+  const router = useRouter();
+
+  const [errorMessage, setErrorMessage] = useState<string | null>();
+
+  async function handleSubmit(formData: FormData) {
+    try {
+      const rawFormData = {
+        email: formData.get("email") as string,
+        password: formData.get("password") as string,
+      };
+
+      const { nextStep } = await signIn({
+        username: rawFormData.email,
+        password: rawFormData.password,
+      });
+
+      if (nextStep.signInStep != "DONE") return;
+
+      router.push("/dashboard");
+    } catch (error) {
+      setErrorMessage(`${error}`);
+    }
+  }
+
   return (
-    <div>
-      <Login></Login>
-    </div>
+    <Container mobileFull={true}>
+      <div className="flex min-h-screen items-center justify-center">
+        <div className="w-full max-w-md rounded-lg bg-white p-6">
+          {errorMessage && (
+            <div className="mb-5 text-center font-semibold text-red-500">
+              {errorMessage}
+            </div>
+          )}
+          <form className="space-y-4" action={handleSubmit}>
+            <input
+              name="email"
+              type="email"
+              placeholder="Email"
+              className="w-full rounded-md bg-gray-100 px-4 py-3 text-sm outline-neutral-800"
+            />
+            <input
+              name="password"
+              type="password"
+              placeholder="Password"
+              className="w-full rounded-md bg-gray-100 px-4 py-3 text-sm outline-neutral-800"
+            />
+            <button
+              type="submit"
+              className="flex w-full items-center justify-center rounded-md bg-neutral-800 px-4 py-3 text-sm font-semibold text-white duration-200 ease-in-out hover:bg-neutral-900 disabled:cursor-not-allowed"
+            >
+              <RiAccountCircleLine className="mr-2 text-xl" />
+              Sign In
+            </button>
+          </form>
+        </div>
+      </div>
+    </Container>
   );
 }
